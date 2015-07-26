be89585f4efcc8c7949bdd07ab5c445b532119e5
Fabio Benigno <benigno.fabio@gmail.com>
2015-07-26 16:37:24 -0400

    Fixed exception when ACM search result does not have abstract
    - Setting the content of the data object to be only the
    title
    
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

--- acm_common_api.py
+++ acm_common_api.py
@@ -4,20 +4,23 @@ from abs_website_common_api import AbsWebsiteCommonApi
 
 class ACMCommonApi(AbsWebsiteCommonApi):
     DATA_SET_SIZE = 445543
-    _THREAD_LIMIT = 1
     QUERY_POOL_FILE_PATH = "/home/fabio/SolrCores/WordLists/new_shine.txt"
+    _THREAD_LIMIT = 1
     _ELEMENT_WITH_NUMBER_MATCHES_TAG = "b"
-    _NO_RESULTS_TAG = "font"
-    _NO_RESULTS_TAG_ATTRIBUTE_VALUE = "+1"
-    _NO_RESULTS_TAG_ATTRIBUTE = "size"
     _BASE_URL = "http://dl.acm.org/results.cfm?query=<<query>>&start=<<offset>>1&dlr=ACM"
     _DATA_FOLDER_PATH = "/media/fabio/FABIO/acm"
     _MAX_RESULTS_PER_PAGE = 20
     _WEB_DOMAIN = "http://dl.acm.org/"
+    _NO_RESULTS_TAG = "font"
+    _NO_RESULTS_TAG_ATTRIBUTE = "size"
+    _NO_RESULTS_TAG_ATTRIBUTE_VALUE = "+1"
     _TITLE_TAG_ID_ATTRIBUTE = "href"
     _TITLE_TAG = "a"
-    _TITLE_TAG_ATTRIBUTE_VALUE = "medium-text"
     _TITLE_TAG_ATTRIBUTE = "class"
+    _TITLE_TAG_ATTRIBUTE_VALUE = "medium-text"
+    _ABSTRACT_TAG = "div"
+    _ABSTRACT_TAG_ATTRIBUTE = "class"
+    _ABSTRACT_TAG_ATTRIBUTE_VALUE = "abstract2"
 
     @property
     def thread_limit(self):
@@ -45,8 +48,9 @@ class ACMCommonApi(AbsWebsiteCommonApi):
     def _extract_data_list_from_soup(self, soup):
         dictionary = {ACMCommonApi._TITLE_TAG_ATTRIBUTE: ACMCommonApi._TITLE_TAG_ATTRIBUTE_VALUE}
         title_tag_list = soup.find_all(ACMCommonApi._TITLE_TAG, dictionary)
-        abstract_list = [x.parent.parent.parent.find("div", {"class": "abstract2"}) for x in title_tag_list]
-        data_list = [self._create_data(x[ACMCommonApi._TITLE_TAG_ID_ATTRIBUTE], x.text, y.text)
+        dictionary = {ACMCommonApi._ABSTRACT_TAG_ATTRIBUTE: ACMCommonApi._ABSTRACT_TAG_ATTRIBUTE_VALUE}
+        abstract_list = [x.parent.parent.parent.find(ACMCommonApi._ABSTRACT_TAG, dictionary) for x in title_tag_list]
+        data_list = [self._create_data(x[ACMCommonApi._TITLE_TAG_ID_ATTRIBUTE], x.text, y)
                      for x, y in zip(title_tag_list, abstract_list)]
         return data_list
 
@@ -65,18 +69,11 @@ class ACMCommonApi(AbsWebsiteCommonApi):
             return -1
         return number_matches
 
-    def _create_data(self, href, text, abstract):
-        identifier = self._format_data_id(str(href))
-        content = self._format_data_content(text, abstract)
+    def _create_data(self, href, title, abstract_tag):
+        identifier = ACMCommonApi._WEB_DOMAIN + href[0:href.find("&")]
+        if abstract_tag is not None:
+            content = str(title) + os.linesep + str(abstract_tag.text)
+        else:
+            content = str(title)
         data = self.factory.create_data(identifier, content)
         return data
-
-    def _format_data_id(self, href):
-        href = str(href[0:href.find("&")])
-        return ACMCommonApi._WEB_DOMAIN + href
-
-    def _format_data_content(self, text, abstract):
-        if abstract is not None:
-            return str(text) + os.linesep + str(abstract)
-        else:
-            return str(text)
