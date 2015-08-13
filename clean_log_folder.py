e08d0ccaa374eca940d7c6c21d0680a92bf7784d
Fabio Pires <benigno.fabio@gmail.com>
2015-08-12 22:20:27 -0400

    Created script to empty all Logs subdirs
    Signed-off-by: Fabio Pires <benigno.fabio@gmail.com>

---
+++ clean_log_folder.py
@@ -0,0 +1,9 @@
+import os
+from os import listdir
+from os.path import isfile, join
+
+
+path = ".\\Logs"
+directories = [x[0] for x in os.walk(path) if x[0] != path]
+files = [[ x for x in listdir(y) if isfile(join(y,x))] for y in directories]
+[[os.unlink(x) for x in y if ".gitignore" not in x] for y in files]
