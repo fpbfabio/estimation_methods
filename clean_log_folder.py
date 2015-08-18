8456387d269b6e0a402f1789abd34efe4e61dc10
Fabio Pires <benigno.fabio@gmail.com>
2015-08-17 21:37:31 -0400

    Fixed clear logs script
    Signed-off-by: Fabio Pires <benigno.fabio@gmail.com>

--- clean_log_folder.py
+++ clean_log_folder.py
@@ -5,5 +5,5 @@ from os.path import isfile, join
 
 path = ".\\Logs"
 directories = [x[0] for x in os.walk(path) if x[0] != path]
-files = [[ x for x in listdir(y) if isfile(join(y,x))] for y in directories]
-[[os.unlink(x) for x in y if ".gitignore" not in x] for y in files]
+files = [[ join(y,x) for x in listdir(y) if isfile(join(y,x))] for y in directories]
+[[os.unlink(x) for x in y if ".gitignore" not in str(x)] for y in files]
